fbed5af6ab903c02dbe3bae7a681a284d0d9ee90
finn1990 <fall1600@gmail.com>
2015-06-25 22:14:03 +0800

    demo_tj page 58

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+    @name = "#{params[:first_name]} #{params[:last_name]} "
+    render layout: 'admin'
+  end
+
+  def math
+    @numbers = (1..46).to_a.sample(6)
+  end
+end
