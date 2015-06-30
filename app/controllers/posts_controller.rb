32d6e27f9d40bd81198b9e0f07e8e809fbd855d2
finn1990 <fall1600@gmail.com>
2015-06-30 08:27:42 +0800

    for system upgrade

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,21 @@
 class PostsController < ApplicationController
+  
+  before_action :find_post, :only => [ :show, :edit, :update, :destroy ]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
   end
 
   def create
-    @post = Post.new title: params[:title], content: params[:content]
+    #@post = Post.new title: params[:title], content: params[:content]
+    @post = Post.new post_params
     if @post.save
       redirect_to @post
     else
@@ -16,9 +23,30 @@ class PostsController < ApplicationController
     end
   end
 
-  def new
-  end
-
   def edit
   end
+
+  def update
+    if @post.update post_params
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  protected
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+  
 end
