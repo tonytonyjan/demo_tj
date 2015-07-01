709ffbdcb7ba04bb80175a9bb4d87b82e779c105
finn1990 <fall1600@gmail.com>
2015-07-01 17:00:35 +0800

    going to delete post tag relationship if post or tag is delete by user

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,11 +1,12 @@
 class TagsController < ApplicationController
 
+  before_action :find_tag, :only => [ :show, :edit, :update, :destroy ]
+
   def index
     @tags = Tag.all
   end
 
   def show
-    @tag = Tag.find(params[:id])
   end
 
   def new
@@ -27,9 +28,23 @@ class TagsController < ApplicationController
   end
 
   def update
+    if @tag.update tag_params
+      flash[:notice] = "編輯標籤成功"
+      redirect_to tag_posts_path(@tag)
+    else
+      flash[:alert] = "編輯標籤失敗"
+      render :edit
+    end
   end
 
+  # delete relationship is not yet
+  # 2015.7.1
   def destroy
+    #relevant_posts = @tag.posts
+    #post_tag_ships = PostTagship.where(:post_id => relevant_posts)
+    #post_tag_ships.destroy
+    @tag.destroy
+    redirect_to tags_path
   end
 
   def posts_of_this_tag
@@ -39,6 +54,10 @@ class TagsController < ApplicationController
   end
 
   protected
+
+  def find_tag
+    @tag = Tag.find(params[:id])
+  end
 
   def tag_params
     params.require(:tag).permit(:name)
